4808b7bea1daa4d075c13a4bd94c31c90bff4a9b
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-06-21 18:59:13 +0600

    Password Generator

--- day-31-Password-Generator/script.js
+++ day-31-Password-Generator/script.js
@@ -14,6 +14,20 @@ const randomFunc = {
     symbol: getRandomSymbol
 }
 
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innerText
+
+    if(!password) { return }
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard!')
+})
+
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value
     const hasLower = lowercaseEl.checked
@@ -21,14 +35,28 @@ generateEl.addEventListener('click', () => {
     const hasNumber = numbersEl.checked
     const hasSymbol = symbolsEl.checked
 
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol)
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
 })
 
 function generatePassword(lower, upper, number, symbol, length) {
-    let generatePassword = ''
+    let generatedPassword = ''
     const typesCount = lower + upper + number + symbol
-    const typeArr = [{lower}, {upper}, {number}, {symbol}]
-    console.log(typeArr);
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
+    
+    if(typesCount === 0) {
+        return ''
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
 }
 
 function getRandomLower() {
@@ -43,9 +71,7 @@ function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
-
 function getRandomSymbol() {
     const symbols = '!@#$%^&*(){}[]=<>/,.'
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
-
