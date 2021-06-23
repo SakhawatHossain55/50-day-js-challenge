7ba5f4cb3bdc17e5eeb8e350285451705f0a0a7d
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-06-23 13:41:33 +0600

    notes app

--- day-33-Notes-App/script.js
+++ day-33-Notes-App/script.js
@@ -1,4 +1,9 @@
 const addBtn = document.getElementById("add");
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note));
+}
 
 addBtn.addEventListener("click", () => addNewNote());
 
@@ -25,6 +30,8 @@ function addNewNote(text = '') {
 
     deleteBtn.addEventListener('click', () => {
         note.remove()
+
+        updateLS()
     })
     editBtn.addEventListener('click', () => {
         main.classList.toggle('hidden')
@@ -35,8 +42,25 @@ function addNewNote(text = '') {
         const {value} = e.target
 
         main.innerHTML = marked(value)
+
+        updateLS()
     })
 
     document.body.appendChild(note)
 }
 
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = [];
+
+    notesText.forEach(note => notes.push(note.value))
+
+    // console.log(notes);
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+
+// 
+// localStorage.removeItem('name')
